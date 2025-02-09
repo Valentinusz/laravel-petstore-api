5d3f21d85013f27243f44d82310a5034e8f2dae8
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2025-02-09 13:45:38 +0100

    update

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -2,20 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePetRequest;
 use App\Http\Resources\PetCollection;
 use App\Http\Resources\PetResource;
 use App\Models\Pet;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use OpenApi\Attributes as OA;
 
 #[OA\Tag(name: "Pet")]
 class PetController extends Controller
 {
-    #[OA\Get(path: '/api/v1/pets', summary: 'Get a page of pets', tags: ["Pet"])]
+    #[OA\Get(path: '/api/v1/pets', operationId: "getPets", summary: 'Get a page of pets', tags: ["Pet"])]
+    #[OA\Parameter(name: 'page', in: 'query', required: false, schema: new OA\Schema(type: 'integer'), example: 1)]
+    #[OA\Parameter(name: 'page_size', in: 'query', required: false, schema: new OA\Schema(type: 'integer'), example: 20)]
     #[OA\Response(response: 200, description: 'OK', content: new OA\MediaType('application/json'))]
-    public function index(): PetCollection
+    public function index(Request $request): PetCollection
     {
-        return new PetCollection(Pet::paginate());
+
+        return new PetCollection(
+            Pet::paginate(
+                perPage: $request->query('page_size', 20),
+                page: $request->query('page', 0)
+            )
+        );
     }
 
     #[OA\Get(path: '/api/v1/pets/{pet}', summary: 'Get the given pet', tags: ["Pet"])]
@@ -28,16 +39,25 @@ class PetController extends Controller
     }
 
     #[OA\Post(path: '/api/v1/pets', summary: 'Add a new pet', tags: ["Pet"])]
+    #[OA\RequestBody(required: true, content: new OA\JsonContent(ref: "#/components/schemas/StorePetRequest"))]
     #[OA\Response(response: 201, description: 'Created', content: new OA\MediaType('application/json'))]
     #[OA\Response(response: 404, description: 'OK')]
-    public function store()
+    public function store(StorePetRequest $request)
     {
+        $validated = $request->validated();
+
+        \Illuminate\Log\log($validated);
+
+        return response()->json([
+            'name' => $validated["name"],
+        ], 201);
     }
 
     #[OA\Put(path: '/api/v1/pets/{pet}', summary: 'Update a pet', tags: ["Pet"])]
     #[OA\Response(response: 200, description: 'Created', content: new OA\MediaType('application/json'))]
     #[OA\Response(response: 404, description: 'OK')]
-    public function edit(Pet $pet): PetResource {
+    public function update(Pet $pet): PetResource
+    {
         return new PetResource($pet);
     }
 
@@ -45,7 +65,8 @@ class PetController extends Controller
     #[OA\Parameter(name: 'petId', in: 'path', required: true, schema: new OA\Schema(type: 'integer'), example: 1)]
     #[OA\Response(response: 204, description: 'No content', content: new OA\MediaType('application/json'))]
     #[OA\Response(response: 404, description: 'Not found')]
-    public function destroy(int $id) {
+    public function destroy(int $id)
+    {
         DB::table("pets")->where('id', $id)->delete();
 
         return response()->noContent();
